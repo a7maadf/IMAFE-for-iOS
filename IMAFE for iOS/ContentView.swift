3dbe6cd79b0d29936b7db12cf21c085b27e58577
a7maadf <a7maadf.github.uawca@slmail.me>
2025-03-05 13:43:54 -0600

    alert messages

--- IMAFE for iOS/ContentView.swift
+++ IMAFE for iOS/ContentView.swift
@@ -9,11 +9,13 @@ import SwiftUI
 import PhotosUI
 
 struct ContentView: View {
-    @State private var selectedImage: UIImage?
+    @State private var selectedImage: Data?
+    @State private var selectedUIImage: UIImage?
     @State private var selectedItem: PhotosPickerItem?
     @State private var secretText: String = ""
-    @State private var passwordText: String = "password"
-
+    @State private var passwordText: String = ""
+    
+    
     
     
     
@@ -26,8 +28,8 @@ struct ContentView: View {
                 
                 // Image picker
                 PhotosPicker(selection: $selectedItem, matching: .images) {
-                    if let selectedImage = selectedImage {
-                        Image(uiImage: selectedImage)
+                    if let selectedUIImage {
+                        Image(uiImage: selectedUIImage) // Use the stored UIImage
                             .resizable()
                             .frame(width: 100, height: 100)
                             .clipShape(RoundedRectangle(cornerRadius: 10))
@@ -38,51 +40,53 @@ struct ContentView: View {
                             .clipShape(RoundedRectangle(cornerRadius: 10))
                     }
                 }
-                .onChange(of: selectedItem) {
+                .onChange(of: selectedItem) {_ in
                     Task {
                         if let data = try? await selectedItem?.loadTransferable(type: Data.self) {
-                            selectedImage = UIImage(data: data)
+                            selectedImage = data  // Store as Data
+                            selectedUIImage = UIImage(data: data) // Convert here
                         }
                     }
                 }
+                
                 
                 Text("*click on the image to change it")
                     .font(.custom("SF Pro Italic", size: 10))
                     .foregroundColor(Color.gray)
                     .opacity(0.5)
-
+                
                 
                 Spacer()
                 
                 ZStack(alignment: .topLeading) {
-                            TextEditor(text: $secretText)
-                                .background(Color(red: 0.47, green: 0.04, blue: 0.72))
-                                .scrollContentBackground(.hidden)
-                                .frame(height: 200)
-                                .cornerRadius(10)
-                                .foregroundColor(Color.white)
-                                .font(.custom("SF Pro Rounded", size: 15))
-                            
-                            if secretText.isEmpty {
-                                Text("your secret goes here...")
-                                    .foregroundColor(Color.white.opacity(0.6))
-                                    .font(.custom("SF Pro Rounded", size: 15))
-                                    .padding(.horizontal, 8)
-                                    .padding(.top, 8)
-                                    .allowsHitTesting(false)
-                            }
-                        }
-                        .onTapGesture {
-                            UIApplication.shared.endEditing() // Call the function to dismiss keyboard
-                        }
-
-
+                    TextEditor(text: $secretText)
+                        .background(Color(red: 0.47, green: 0.04, blue: 0.72))
+                        .scrollContentBackground(.hidden)
+                        .frame(height: 200)
+                        .cornerRadius(10)
+                        .foregroundColor(Color.white)
+                        .font(.custom("SF Pro Rounded", size: 15))
+                    
+                    if secretText.isEmpty {
+                        Text("your secret goes here... (ONLY FILL DURING ENCRYPTION, DECRIPTED TEXT WILL BE DISPLAYED HERE)")
+                            .foregroundColor(Color.white.opacity(0.6))
+                            .font(.custom("SF Pro Rounded", size: 15))
+                            .padding(.horizontal, 8)
+                            .padding(.top, 8)
+                            .allowsHitTesting(false)
+                    }
+                }
+                .onTapGesture {
+                    UIApplication.shared.endEditing() // Call the function to dismiss keyboard
+                }
                 
                 
-//                Text("Password")
-//                    .font(.custom("SF Pro Rounded", size: 15))
-//                    .foregroundColor(Color(red: 0.90, green: 0.54, blue: 0.99, opacity: 1.00))
-//                    .frame(maxWidth: .infinity, alignment: .leading) // LtR alignment
+                
+                
+                //                Text("Password")
+                //                    .font(.custom("SF Pro Rounded", size: 15))
+                //                    .foregroundColor(Color(red: 0.90, green: 0.54, blue: 0.99, opacity: 1.00))
+                //                    .frame(maxWidth: .infinity, alignment: .leading) // LtR alignment
                 
                 
                 SecureField("Enter your password here..", text: $passwordText)
@@ -93,63 +97,89 @@ struct ContentView: View {
                     .font(.custom("SF Pro Rounded", size: 15))
                     .onTapGesture {
                         UIApplication.shared.endEditing() // Call the function to dismiss keyboard
-                            }
-                    
-
-
+                    }
                 
                 
-                Button("Save") {
-                    if let imageData = encryptImage(image: selectedImage!, text: secretText, password: passwordText) {
-                        let tempURL = FileManager.default.temporaryDirectory.appendingPathComponent("hidden_message_image.png")
-                        
-                        do {
-                            try imageData.write(to: tempURL) // Save file temporarily
-                            let activityVC = UIActivityViewController(activityItems: [tempURL], applicationActivities: nil)
-                            UIApplication.shared.windows.first?.rootViewController?.present(activityVC, animated: true)
-                        } catch {
-                            print("Error saving image: \(error)")
+                
+                
+                
+                HStack {
+                    Button("Encrypt") {
+                        if selectedUIImage == nil {
+                            showErrorAlert(message: "Choose an image to encrypt")
+                        } else if secretText.isEmpty {
+                            showErrorAlert(message: "Enter a secret text to encrypt")
+                        } else if passwordText.isEmpty {
+                            showErrorAlert(message: "Enter a password to encrypt")
                         }
-                        
-                        
-                        if let extractedText = extractAndDecryptText(from: imageData, password: passwordText) {
-                            print("Decrypted text: \(extractedText)")
-                        } else {
-                            print("Failed to extract or decrypt text.")
+                        else {
+                            if let imageData = encryptImage(image: selectedUIImage!, text: secretText, password: passwordText),
+                               let image = UIImage(data: imageData) {  // Convert Data back to UIImage
+                                UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil) // Save to Photos
+                            } else {
+                                print("Failed to generate encrypted image.")
+                            }
                         }
                     }
+                    .foregroundColor(Color.white)
+                    .frame(width: 100, height: 50)
+                    .background(Color(red: 0.47, green: 0.04, blue: 0.72))
+                    .cornerRadius(10)
+                    .font(.custom("SF Pro Rounded", size: 15))
+                    .foregroundColor(Color(red: 0.90, green: 0.54, blue: 0.99, opacity: 1.00))
+                    
+                    
+                    
+                    Button("Decrypt") {
+                        if selectedUIImage == nil {
+                            showErrorAlert(message: "Choose an image to decrypt")
+                        } else {
+                            if let extractedText = extractAndDecryptText(from: selectedImage!, password: passwordText) {
+                                secretText = extractedText
+                            } else {
+                                showErrorAlert(message: "Wrong password! Try again")
+                            }
+                        }
+                        
+                    }
+                    
+                    
+                    
+                    .foregroundColor(Color.white)
+                    .frame(width: 100, height: 50)
+                    .background(Color(red: 0.47, green: 0.04, blue: 0.72))
+                    .cornerRadius(10)
+                    .font(.custom("SF Pro Rounded", size: 15))
+                    .foregroundColor(Color(red: 0.90, green: 0.54, blue: 0.99, opacity: 1.00))
                 }
-                
-                .foregroundColor(Color.white)
-                .frame(width: 100, height: 50)
-                .background(Color(red: 0.47, green: 0.04, blue: 0.72))
-                .cornerRadius(10)
-                .font(.custom("SF Pro Rounded", size: 15))
-                .foregroundColor(Color(red: 0.90, green: 0.54, blue: 0.99, opacity: 1.00))
                 
                 
                 Spacer()
-            
+                
             }
             
             .padding([.top, .leading, .trailing])
             .keyboardResponsive()
             
-                
+            
             
         }
         .ignoresSafeArea()
         
         .onTapGesture {
-                    UIApplication.shared.endEditing() // Dismiss keyboard
-                }
+            UIApplication.shared.endEditing() // Dismiss keyboard
+        }
         
-
+        
         
         
     }
-        
+    
 }
+
+
+
+
 
 
 extension UIApplication {
--- IMAFE for iOS/Models/alertsFunctions.swift
+++ IMAFE for iOS/Models/alertsFunctions.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import UIKit
+
+
+
+func showErrorAlert(message: String) {
+    let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+    
+    // Make sure this runs on a view controller
+    if let topVC = UIApplication.shared.windows.first?.rootViewController {
+        topVC.present(alert, animated: true, completion: nil)
+    }
+}
